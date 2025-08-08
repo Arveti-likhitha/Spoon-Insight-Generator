d0ac0fe3d498e4251ca1487686b21eeb302fc902
likhithaarveti <likhithaarveti@gmail.com>
2025-08-08 17:00:21 +0530

    fix: vite base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', //Needed for correct asset loading in Vercel static hosting
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
